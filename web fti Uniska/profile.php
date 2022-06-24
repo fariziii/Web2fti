2d7d9852664b37daf844973eecc96a2d9d1c097d
fariziii <farizkosongdualima@gmail.com>
2022-06-24 18:59:59 +0800

    Web FTI Uniska

---
+++ web fti Uniska/profile.php
@@ -0,0 +1,54 @@
+
+<div class="row">
+    <div class="col-md-12">
+    <div class="text-center">
+        <img src="img/farizi.jpeg" alt="fariz" width="160" class="rounded-circle">
+ <table class="table mt-4" style="width:100%; margin:0 auto;">
+ <tbody>
+ <tr>
+    <td align="right" width="50%"><strong>NIM:</strong></td>
+    <td align="left" width="50%">2010010288</td>
+ </tr>
+ <tr>
+    <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+    <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+ </tr>
+ <tr>
+    <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+    <td align="left" width="50%">Muhammad Maulana Alfarizi</td>
+</tr>
+ <tr>
+    <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+    <td align="left" width="50%">Tanah laut, 30 Mei 2001</td>
+ </tr>
+ <tr>
+    <td align="right" width="50%"><strong>Telepon :</strong></td>
+    <td align="left" width="50%">0821 5105 0379</td>
+ </tr>
+ <tr>
+    <td align="right" width="50%"><strong>email :</strong></td>
+    <td align="left" width="50%">farizkosongdualima@gmail.com</td>
+ </tr>
+ <tr>
+    <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+    <td align="left" width="50%">
+        <a href="https://www.facebook.com/si.limadua/" target="_blank" style="text-decoration:none;">
+            <img src="img/fb.svg" width="30" alt="facebook">
+        </a>
+        <a href="https://www.instagram.com/maulanalfarizi/" target="_blank" style="text-decoration:none;">
+            <img src="img/ig.svg" width="30" alt="instagram">
+        </a>
+        <a href="http://twitter.com/" target="_blank" style="text-decoration:none;">
+             <img src="img/twitter.svg" width="30" alt="twitter">
+        </a>
+        <a href="https://github.com/fariziii" target="_blank">
+            <img src="img/25231.png" width="30" alt="github">
+         </a>
+     </td>
+    </tr>
+</tbody>
+</table>
+</div>
+</div>
+</div>
+<?php include "footer.php" ?>
