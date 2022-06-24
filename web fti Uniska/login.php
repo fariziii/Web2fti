23708ad4f461ca1ebe5fb42a88b5d2b834f4bc1b
Muhammad Maulana Alfarizi <92573073+fariziii@users.noreply.github.com>
2022-06-24 20:22:13 +0800

    Update login.php

--- web fti Uniska/login.php
+++ web fti Uniska/login.php
@@ -1,7 +1,6 @@
 <?php
 
 if (isset($_POST['login'])) {
-    header("location:admin/index.php");
   $username   = $_POST['username'];
   $pass       = $_POST['password'];
 
@@ -47,7 +46,7 @@ if (isset($_POST['login'])) {
         <?php if (isset($error)) : ?>
 
           <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <strong>Login gagal</strong> Periksa kembali Username dan Password
+            <strong>Login gagal</strong> kayanya username dan passwordnya salah.......
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
